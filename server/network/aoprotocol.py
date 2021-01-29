8a675482ec57d8f76f7ff682705251b53a1eb797
Caleb Mabry <36182383+caleb-mabry@users.noreply.github.com>
2021-01-28 19:37:19 -0500

    Fixes issue 101

--- server/network/aoprotocol.py
+++ server/network/aoprotocol.py
@@ -627,12 +627,12 @@ class AOProtocol(asyncio.Protocol):
                 "Your message is a repeat of the last one. Don't spam!")
             return
 
+        if evidence not in self.client.evi_list:
+            evidence = 0
         # Reveal evidence to everyone if hidden
-        if evidence:
-            if self.client.area.evi_list.evidences[
-                    self.client.evi_list[evidence] - 1].pos != 'all':
-                self.client.area.evi_list.evidences[
-                    self.client.evi_list[evidence] - 1].pos = 'all'
+        elif evidence:
+            if self.client.area.evi_list.evidences[self.client.evi_list[evidence] - 1].pos != 'all':
+                self.client.area.evi_list.evidences[self.client.evi_list[evidence] - 1].pos = 'all'
                 self.client.area.broadcast_evidence_list()
 
         # Here, we check the pair stuff, and save info about it to the client.
